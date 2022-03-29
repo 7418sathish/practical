13016fa1ee76e27ae606ddf39d195be72f415f71
Sathish kumar P <98468288+7418sathish@users.noreply.github.com>
2022-03-29 15:45:38 +0530

    test button

---
+++ new.php
@@ -0,0 +1,18 @@
+<?php
+
+define('name','sathish');
+//$name = "sathish";
+
+//echo $name;
+
+
+?>
+
+<html>
+    <head></head>
+    <body>
+        <form class="form" action="#" method="get">
+        <input type="input" class="input" name="name">
+        <input type="submit"class="submit" name="submit">
+    </body>
+</html>
